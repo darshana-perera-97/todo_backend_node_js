9ff9a21a5b34f199bd8c53c9ee3cc944db40be22
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-01 09:23:03 +0530

    database name added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 
-mongoose.connect("mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/todo_new?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
   console.log("Server runs in 3001");
