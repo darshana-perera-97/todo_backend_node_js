093fe5805cd1a4f3ba8549f06e72f8896c7b0cd9
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-01 09:19:11 +0530

    database connected to backend

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
+
+mongoose.connect("mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/?retryWrites=true&w=majority");
 
 app.listen(3001, () => {
   console.log("Server runs in 3001");
